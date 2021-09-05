54929e34eda116fbf6d3a5b67b35c8bf084c9546
Krash <krash.munoz@gmail.com>
2021-09-05 13:32:12 -0400

    Added directory component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { DirectoryComponent } from './directory/directory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavigationComponent
+    TopNavigationComponent,
+    DirectoryComponent
   ],
   imports: [
     BrowserModule,
