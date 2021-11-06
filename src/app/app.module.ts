9538c76e8a4013332e69e069f62e71a02d9211c6
Krash <krash.munoz@gmail.com>
2021-11-06 17:49:28 -0400

    Added firestore module to app.ts and formatted angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
     BrowserAnimationsModule,
     FormsModule,
     AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
