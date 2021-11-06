0d9253184b290eb3dc7ebeb06fab89a53416524d
Krash <krash.munoz@gmail.com>
2021-11-06 16:15:27 -0400

    Updated environment file with firebase config and tried to update angular core and cli

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAM8-eHXd5Rb5oSbUNxy4O416yO05p0u6k",
+    authDomain: "directory-noir.firebaseapp.com",
+    projectId: "directory-noir",
+    storageBucket: "directory-noir.appspot.com",
+    messagingSenderId: "651163044140",
+    appId: "1:651163044140:web:594df1996b7b04342a8489",
+    measurementId: "G-P5FZPK0358"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAM8-eHXd5Rb5oSbUNxy4O416yO05p0u6k",
+    authDomain: "directory-noir.firebaseapp.com",
+    projectId: "directory-noir",
+    storageBucket: "directory-noir.appspot.com",
+    messagingSenderId: "651163044140",
+    appId: "1:651163044140:web:594df1996b7b04342a8489",
+    measurementId: "G-P5FZPK0358"
+  }
 };
 
 /*
